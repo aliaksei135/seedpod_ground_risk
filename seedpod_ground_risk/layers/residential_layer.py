c957db76b3590b0db52098352586ba5ea82ff121
Aliaksei Pilko <a.pilko@soton.ac.uk>
2020-12-23 16:12:15 +0000

    Fix incorrect CRS being set

--- seedpod_ground_risk/layers/residential_layer.py
+++ seedpod_ground_risk/layers/residential_layer.py
@@ -90,8 +90,7 @@ class ResidentialLayer(Layer):
         """
         # Import Census boundaries in Ordnance Survey grid and reproject
         census_wards_df = gpd.read_file(os.sep.join(('static_data', 'england_wa_2011_clipped.shp'))).drop(
-            ['altname', 'oldcode'], axis=1).set_crs(
-            'EPSG:27700').to_crs('EPSG:4326')
+            ['altname', 'oldcode'], axis=1).to_crs('EPSG:4326')
         # Import census ward densities
         density_df = pd.read_csv(os.sep.join(('static_data', 'density.csv')), header=0)
         # Scale from hectares to m^2
