d98b0a100107ea8f8ac371b26f4df398cfba6748
Aliaksei Pilko <a.pilko@soton.ac.uk>
2021-01-01 20:07:49 +0000

    Add euclidean risk heuristic

--- seedpod_ground_risk/pathfinding/heuristic.py
+++ seedpod_ground_risk/pathfinding/heuristic.py
@@ -20,5 +20,17 @@ class ManhattanHeuristic(Heuristic):
 
 
 class EuclideanRiskHeuristic(Heuristic):
+    from seedpod_ground_risk.pathfinding.environment import Environment
+
+    def __init__(self, environment: Environment):
+        from seedpod_ground_risk.pathfinding.a_star import AStar
+
+        self.environment = environment
+        self.pathfinder = AStar()
+
     def h(self, node: Node, goal: Node):
-        pass
+        path_to_goal = self.pathfinder.find_path(self.environment, node, goal)
+        cumulative_val = 0
+        for node in path_to_goal:
+            cumulative_val += node.n
+        return cumulative_val
