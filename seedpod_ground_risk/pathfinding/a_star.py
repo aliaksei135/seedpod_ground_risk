9034c9ed774f6dc20bdc533e8cb8f88b6ddcb149
Aliaksei Pilko <a.pilko@soton.ac.uk>
2021-01-01 14:49:36 +0000

    Fixes for A*

--- seedpod_ground_risk/pathfinding/a_star.py
+++ seedpod_ground_risk/pathfinding/a_star.py
@@ -1,5 +1,5 @@
 from queue import PriorityQueue
-from typing import List, Dict
+from typing import List, Dict, Union
 
 from seedpod_ground_risk.pathfinding.algorithm import Algorithm
 from seedpod_ground_risk.pathfinding.environment import Environment, Node
@@ -7,17 +7,17 @@ from seedpod_ground_risk.pathfinding.heuristic import Heuristic, EuclideanHeuris
 
 
 class AStar(Algorithm):
-    def __init__(self, heuristic: Heuristic = EuclideanHeuristic):
+    def __init__(self, heuristic: Heuristic = EuclideanHeuristic()):
         self.heuristic = heuristic.h
 
-    def find_path(self, environment: Environment, start: Node, end: Node) -> List[Node]:
+    def find_path(self, environment: Environment, start: Node, end: Node) -> Union[List[Node], None]:
         open = PriorityQueue()
         open.put((0, start), False)
         closed = {start: None}
         costs = {start: 0}
 
         while not open.empty():
-            node = open.get()
+            node = open.get()[1]
             if node == end:
                 return self._reconstruct_path(end, closed)
 
@@ -26,16 +26,16 @@ class AStar(Algorithm):
                 cost = current_cost + environment.f_cost(node, neighbour)
                 if neighbour not in costs or costs[neighbour] > cost:
                     costs[neighbour] = cost
-                    open.put((cost + self.heuristic(node, end), neighbour))
+                    open.put((cost + self.heuristic(neighbour, end), neighbour))
                     closed[neighbour] = node
+        return None
 
     def _reconstruct_path(self, end: Node, closed_list: Dict[Node, Node]) -> List[Node]:
-        reverse_closed = {v: k for k, v in closed_list.items()}
         reverse_path = []
         reverse_path_append = reverse_path.append
         reverse_path_append(end)
-        node = reverse_closed[end]
+        node = closed_list[end]
         while node is not None:
             reverse_path_append(node)
-            node = reverse_closed[node]
+            node = closed_list[node]
         return list(reversed(reverse_path))
