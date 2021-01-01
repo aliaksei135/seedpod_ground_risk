fd20f40a7b20430f000976048496db44b6bd7c26
Aliaksei Pilko <a.pilko@soton.ac.uk>
2021-01-01 14:50:06 +0000

    Pathfinding Tests

---
+++ tests/pathfinding/test_grid_environment.py
@@ -0,0 +1,33 @@
+import unittest
+
+from seedpod_ground_risk.pathfinding.environment import GridEnvironment
+from tests.pathfinding.test_data import SMALL_TEST_GRID, LARGE_TEST_GRID, SMALL_TEST_GRAPH_NO_DIAGONAL
+
+
+class GridEnvironmentNoDiagonalsTestCase(unittest.TestCase):
+
+    def setUp(self) -> None:
+        super().setUp()
+        self.small_environment = GridEnvironment(SMALL_TEST_GRID, diagonals=False)
+        self.large_environment = GridEnvironment(LARGE_TEST_GRID, diagonals=False)
+
+    def test_grid_to_graph(self):
+        self.maxDiff = None
+        graph = self.small_environment._generate_graph()
+        self.assertEqual(len(graph), SMALL_TEST_GRID.shape[0] * SMALL_TEST_GRID.shape[1])
+        self.assertDictEqual(SMALL_TEST_GRAPH_NO_DIAGONAL, graph)
+
+
+class GridEnvironmentWithDiagonalsTestCase(unittest.TestCase):
+
+    def setUp(self) -> None:
+        super().setUp()
+        self.small_environment = GridEnvironment(SMALL_TEST_GRID, diagonals=True)
+        self.large_environment = GridEnvironment(LARGE_TEST_GRID, diagonals=True)
+
+    def test_grid_to_graph(self):
+        self.assertEqual(True, False)
+
+
+if __name__ == '__main__':
+    unittest.main()
