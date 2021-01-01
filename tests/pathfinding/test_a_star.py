b998c3874b53ceccb8d09dcb7ecce3c133fdd9de
Aliaksei Pilko <a.pilko@soton.ac.uk>
2021-01-01 20:06:56 +0000

    Ad A* tests

--- tests/pathfinding/test_a_star.py
+++ tests/pathfinding/test_a_star.py
@@ -2,34 +2,65 @@ import unittest
 
 from seedpod_ground_risk.pathfinding.a_star import AStar
 from seedpod_ground_risk.pathfinding.environment import GridEnvironment, Node
-from seedpod_ground_risk.pathfinding.heuristic import EuclideanHeuristic
+from seedpod_ground_risk.pathfinding.heuristic import EuclideanRiskHeuristic
 from tests.pathfinding.test_data import SMALL_TEST_GRID
 
 
 class AStarTestCase(unittest.TestCase):
 
-    def test_no_diagonals(self):
-        environment = GridEnvironment(SMALL_TEST_GRID, diagonals=False)
-        algo = AStar()
-        start = Node(0, 0, 0)
-        end = Node(4, 4, 0)
-        path = algo.find_path(environment, start, end)
-        self.assertEqual(path, [])
+    def setUp(self) -> None:
+        super().setUp()
+        self.diag_environment = GridEnvironment(SMALL_TEST_GRID, diagonals=True)
+        self.no_diag_environment = GridEnvironment(SMALL_TEST_GRID, diagonals=False)
+        self.algo = AStar(heuristic=EuclideanRiskHeuristic(self.no_diag_environment))
+        self.start = Node(0, 0, 0)
+        self.end = Node(4, 4, 0)
 
-    def test_with_diagonals(self):
-        environment = GridEnvironment(SMALL_TEST_GRID, diagonals=True)
-        algo = AStar(heuristic=EuclideanHeuristic)
-        start = Node(0, 0, 0)
-        end = Node(4, 4, 0)
-        path = algo.find_path(environment, start, end)
-        self.assertEqual(path, [])
+    def test_direct_no_diagonals(self):
+        """
+        Test simplest case of direct path on small grid with no diagonals ignoring node values
+        """
+        path = self.algo.find_path(self.no_diag_environment, self.start, self.end)
+
+        self.assertEqual(path[0], self.start, 'Start node not included in path')
+        self.assertEqual(path[-1], self.end, 'Goal node not included in path')
+        self.assertEqual(path, [
+            Node(0, 0, 0),
+            Node(1, 0, 1),
+            Node(1, 1, 5),
+            Node(2, 1, 12),
+            Node(2, 2, 45),
+            Node(2, 3, 30),
+            Node(3, 3, 30),
+            Node(3, 4, 2),
+            Node(4, 4, 0)
+        ],
+                         "Incorrect path")
+
+    def test_direct_with_diagonals(self):
+        """
+        Test simplest case of direct path on small grid with diagonals ignoring node values
+        """
+        path = self.algo.find_path(self.diag_environment, self.start, self.end)
+
+        self.assertEqual(path[0], self.start, "Start node not included in path")
+        self.assertEqual(path[-1], self.end, 'Goal node not included in path')
+        self.assertEqual(path, [
+            Node(0, 0, 0),
+            Node(1, 1, 5),
+            Node(2, 2, 45),
+            Node(3, 3, 30),
+            Node(4, 4, 0)
+        ],
+                         "Incorrect path")
 
     def test_start_is_goal(self):
-        environment = GridEnvironment(SMALL_TEST_GRID, diagonals=False)
-        algo = AStar(heuristic=EuclideanHeuristic())
-        start = end = Node(0, 0, 0)
-        path = algo.find_path(environment, start, end)
-        self.assertEqual(path, [])
+        """
+        Test case of start and goal being the same node
+        """
+        path = self.algo.find_path(self.no_diag_environment, self.start, self.start)
+
+        self.assertEqual(path, [self.start])
 
     def test_goal_unreachable(self):
         pass
