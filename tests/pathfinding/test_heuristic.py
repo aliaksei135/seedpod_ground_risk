c952dfcf191df79e5904973880345d4f4d2187c7
Aliaksei Pilko <a.pilko@soton.ac.uk>
2021-01-02 10:47:53 +0000

    Fix manhattan heuristic mixed sign handling

--- tests/pathfinding/test_heuristic.py
+++ tests/pathfinding/test_heuristic.py
@@ -1,7 +1,7 @@
 import unittest
 
 from seedpod_ground_risk.pathfinding.environment import Node
-from seedpod_ground_risk.pathfinding.heuristic import EuclideanHeuristic
+from seedpod_ground_risk.pathfinding.heuristic import EuclideanHeuristic, ManhattanHeuristic
 
 
 class EuclideanHeuristicTestCase(unittest.TestCase):
@@ -41,8 +41,39 @@ class EuclideanHeuristicTestCase(unittest.TestCase):
 
 
 class ManhattanHeuristicTestCase(unittest.TestCase):
-    def test_heuristic(self):
-        pass
+    def setUp(self) -> None:
+        super().setUp()
+        self.heuristic = ManhattanHeuristic().h
+
+    def test_positive(self):
+        """
+        Test both dimension differences being positive
+        """
+        n1 = Node(0, 0, 0)
+        n2 = Node(1, 1, 0)
+        cost = self.heuristic(n1, n2)
+        self.assertEqual(cost, 2, 'Wrong cost')
+
+    def test_negative(self):
+        """
+        Test both dimensions being negative
+        """
+        n1 = Node(0, 0, 0)
+        n2 = Node(-1, -1, 0)
+        cost = self.heuristic(n1, n2)
+        self.assertEqual(cost, 2, 'Wrong cost')
+
+    def test_mixed(self):
+        """
+        Test mixed sign dimensions
+        """
+        n1 = Node(0, 0, 0)
+        n2 = Node(-1, 1, 0)
+        n3 = Node(1, -1, 0)
+        cost2 = self.heuristic(n1, n3)
+        cost1 = self.heuristic(n1, n2)
+        self.assertEqual(cost1, cost2, "Wrong cost")
+        self.assertEqual(cost1, 2, 'Wrong cost')
 
 
 class EuclideanRiskHeuristicTestCase(unittest.TestCase):
