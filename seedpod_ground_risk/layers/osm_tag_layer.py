6922d46e69eb8133f152b2be47c8d76b184be929
Aliaksei Pilko <a.pilko@soton.ac.uk>
2021-01-07 18:22:37 +0000

    Generify OSM tag layer

---
+++ seedpod_ground_risk/layers/osm_tag_layer.py
@@ -0,0 +1,69 @@
+import abc
+from typing import NoReturn
+
+import geopandas as gpd
+import shapely.geometry as sg
+
+from seedpod_ground_risk.layers.data_layer import DataLayer
+
+
+class OSMTagLayer(DataLayer, abc.ABC):
+    _landuse_polygons: gpd.GeoDataFrame
+
+    def __init__(self, key, rasterise: bool = False, osm_tag: str = 'landuse=residential'):
+        super().__init__(key, rasterise)
+        self._osm_tag = osm_tag
+        self._landuse_polygons = gpd.GeoDataFrame()
+
+    def query_osm_polygons(self, bound_poly: sg.Polygon) -> NoReturn:
+        """
+        Perform blocking query on OpenStreetMaps Overpass API for objects with the passed landuse.
+        Retain only polygons and store in GeoPandas GeoDataFrame
+        :param shapely.Polygon bound_poly: bounding box around requested area in EPSG:4326 coordinates
+        :param str landuse: OSM landuse key from https://wiki.openstreetmap.org/wiki/Landuse
+        """
+        from time import time
+        import requests
+
+        t0 = time()
+        bounds = bound_poly.bounds
+        overpass_url = "http://overpass-api.de/api/interpreter"
+        query = """
+                  [out:json]
+                  [timeout:120]
+                  [bbox:{s_bound}, {w_bound}, {n_bound}, {e_bound}];
+                  (
+                      node[{tag}];
+                      way[{tag}];
+                      rel[{tag}];
+                  ); 
+                  out center body;
+                  >;
+                  out center qt;
+              """.format(tag=self._osm_tag,
+                         s_bound=bounds[0], w_bound=bounds[1], n_bound=bounds[2], e_bound=bounds[3])
+        resp = requests.get(overpass_url, params={'data': query})
+        data = resp.json()
+        print("OSM query took ", time() - t0)
+
+        ways = [o for o in data['elements'] if o['type'] == 'way']
+        nodes = {o['id']: (o['lon'], o['lat']) for o in data['elements'] if o['type'] == 'node'}
+
+        df_list = []
+        # Iterate polygons ways
+        for element in ways:
+            # Find the vertices (AKA nodes) that make up each polygon
+            locs = [nodes[id] for id in element['nodes']]
+            # Not a polygon if less than 3 vertices, so ignore
+            if len(locs) < 3:
+                continue
+            # Add Shapely polygon to list
+            poly = sg.Polygon(locs)
+            df_list.append([poly])
+        # df_list = [sg.Polygon([nodes[id] for id in element['nodes']]) for element in ways]
+        assert len(df_list) > 0
+        # OSM uses Web Mercator so set CRS without projecting as CRS is known
+        poly_df = gpd.GeoDataFrame(df_list, columns=['geometry']).set_crs('EPSG:4326')
+
+        self._landuse_polygons = self._landuse_polygons.append(poly_df)
+        self._landuse_polygons.drop_duplicates(subset='geometry', inplace=True, ignore_index=True)
--- seedpod_ground_risk/layers/residential_layer.py
+++ seedpod_ground_risk/layers/residential_layer.py
@@ -1,25 +1,22 @@
-from typing import NoReturn, Optional
+from typing import NoReturn
 
 import geopandas as gpd
 import shapely.geometry as sg
 from holoviews.element import Geometry
 from shapely import speedups
 
-from seedpod_ground_risk.layers.data_layer import DataLayer
+from seedpod_ground_risk.layers.osm_tag_layer import OSMTagLayer
 
 gpd.options.use_pygeos = True  # Use GEOS optimised C++ routines
 speedups.enable()  # Enable shapely speedups
 
 
-class ResidentialLayer(DataLayer):
+class ResidentialLayer(OSMTagLayer):
     _census_wards: gpd.GeoDataFrame
-    _landuse_polygons: gpd.GeoDataFrame
 
     def __init__(self, key, **kwargs):
-        super(ResidentialLayer, self).__init__(key, **kwargs)
-
+        super(ResidentialLayer, self).__init__(key, osm_tag='landuse=residential', **kwargs)
         self._census_wards = gpd.GeoDataFrame()
-        self._landuse_polygons = gpd.GeoDataFrame()
 
     def preload_data(self):
         print("Preloading Residential Layer")
@@ -38,7 +35,7 @@ class ResidentialLayer(DataLayer):
         bounds = bounds_polygon.bounds
 
         if not from_cache:
-            self.query_osm_landuse_polygons(bounds_polygon)
+            self.query_osm_polygons(bounds_polygon)
         bounded_census_wards = self._census_wards.cx[bounds[1]:bounds[3], bounds[0]:bounds[2]]
         print("Residential: Bounded census wards cumtime ", time() - t0)
 
@@ -108,59 +105,3 @@ class ResidentialLayer(DataLayer):
 
         # These share a common UID, so merge together on it and store
         self._census_wards = census_wards_df.merge(density_df, on='code')
-
-    def query_osm_landuse_polygons(self, bound_poly: sg.Polygon, landuse: Optional[str] = 'residential') -> NoReturn:
-        """
-        Perform blocking query on OpenStreetMaps Overpass API for objects with the passed landuse.
-        Retain only polygons and store in GeoPandas GeoDataFrame
-        :param shapely.Polygon bound_poly: bounding box around requested area in EPSG:4326 coordinates
-        :param str landuse: OSM landuse key from https://wiki.openstreetmap.org/wiki/Landuse
-        """
-        from time import time
-        import requests
-
-        t0 = time()
-        bounds = bound_poly.bounds
-        overpass_url = "http://overpass-api.de/api/interpreter"
-        query = """
-               [out:json]
-               [timeout:120]
-               [bbox:{s_bound}, {w_bound}, {n_bound}, {e_bound}];
-               (
-                   node[landuse={landuse}];
-                   way[landuse={landuse}];
-                   rel[landuse={landuse}];
-               ); 
-               out center body;
-               >;
-               out center qt;
-           """.format(landuse=landuse,
-                      s_bound=bounds[0], w_bound=bounds[1], n_bound=bounds[2], e_bound=bounds[3])
-        resp = requests.get(overpass_url, params={'data': query})
-        data = resp.json()
-        print("OSM query took ", time() - t0)
-
-        ways = [o for o in data['elements'] if o['type'] == 'way']
-        nodes = {o['id']: (o['lon'], o['lat']) for o in data['elements'] if o['type'] == 'node'}
-
-        df_list = []
-        # Iterate polygons ways
-        for element in ways:
-            # Find the vertices (AKA nodes) that make up each polygon
-            locs = [nodes[id] for id in element['nodes']]
-            # Not a polygon if less than 3 vertices, so ignore
-            if len(locs) < 3:
-                continue
-            # Add Shapely polygon to list
-            poly = sg.Polygon(locs)
-            df_list.append([poly])
-        # df_list = [sg.Polygon([nodes[id] for id in element['nodes']]) for element in ways]
-        assert len(df_list) > 0
-        # OSM uses Web Mercator so set CRS without projecting as CRS is known
-        poly_df = gpd.GeoDataFrame(df_list, columns=['geometry']).set_crs('EPSG:4326')
-
-        if landuse not in self._landuse_polygons:
-            self._landuse_polygons = poly_df
-        else:
-            self._landuse_polygons = self._landuse_polygons.append(poly_df)
-            self._landuse_polygons.drop_duplicates(subset='geometry', inplace=True, ignore_index=True)
