5e58568f3fb6064c6ac89d4d493596923e08a37f
Aliaksei Pilko <a.pilko@soton.ac.uk>
2021-02-05 08:53:12 +0000

    Add JumpPointSearchAStar unittests

--- tests/pathfinding/test_a_star.py
+++ tests/pathfinding/test_a_star.py
@@ -125,6 +125,102 @@ class RiskGridAStarTestCase(AStarTestCase):
         self.assertEqual(path, None, "Impossible path should be None")
 
 
+class JumpPointSearchAStarTestCase(AStarTestCase):
+
+    def setUp(self) -> None:
+        super().setUp()
+        self.algo = JumpPointSearchAStar()
+
+    def test_direct_no_diagonals(self):
+        """
+        Test simplest case of direct path on small grid with no diagonals ignoring node values
+        """
+        # path = self.algo.find_path(self.small_no_diag_environment, self.start, self.end)
+        self.assertRaises(TypeError, self.algo.find_path, self.small_no_diag_environment, self.start, self.end)
+
+        # self.assertEqual(path[0], self.start, 'Start node not included in path')
+        # self.assertEqual(path[-1], self.end, 'Goal node not included in path')
+        # # Could take either zigzag path both of which are correct but have same path length
+        # # There are no other paths of length 9 other than these zigzag paths, so tests for either of these
+        # self.assertEqual(len(path), 9, 'Path wrong length (not direct?)')
+
+    def test_direct_with_diagonals(self):
+        """
+        Test simplest case of direct path on small grid with diagonals ignoring node values
+        """
+        path = self.algo.find_path(self.small_diag_environment, self.start, self.end)
+
+        self.assertEqual(path[0], self.start, "Start node not included in path")
+        self.assertEqual(path[-1], self.end, 'Goal node not included in path')
+        self.assertEqual(path, [
+            Node(0, 0, 0),
+            Node(4, 4, 0)
+        ],
+                         "Incorrect path")
+
+    def test_start_is_goal(self):
+        """
+        Test case of start and goal being the same node
+        """
+        path = self.algo.find_path(self.small_diag_environment, self.start, self.start)
+
+        self.assertEqual(path, [self.start])
+
+    def test_goal_unreachable(self):
+        """
+        Test behaviour when path is impossible due to obstacles
+        """
+        path = self.algo.find_path(self.small_deadend_environment, self.start, self.end)
+
+        self.assertEqual(path, None, "Impossible path should be None")
+
 
 if __name__ == '__main__':
     unittest.main()
+
+
+class RiskJumpPointSearchAStarTestCase(AStarTestCase):
+
+    def setUp(self) -> None:
+        super().setUp()
+        self.algo = RiskJumpPointSearchAStar()
+
+    def test_direct_no_diagonals(self):
+        """
+        Test simplest case of direct path on small grid with no diagonals ignoring node values
+        """
+        path = self.algo.find_path(self.small_no_diag_environment, self.start, self.end)
+
+        self.assertEqual(path[0], self.start, 'Start node not included in path')
+        self.assertEqual(path[-1], self.end, 'Goal node not included in path')
+        # Could take either zigzag path both of which are correct but have same path length
+        # There are no other paths of length 9 other than these zigzag paths, so tests for either of these
+        self.assertEqual(len(path), 9, 'Path wrong length (not direct?)')
+
+    def test_direct_with_diagonals(self):
+        """
+        Test simplest case of direct path on small grid with diagonals ignoring node values
+        """
+        path = self.algo.find_path(self.small_diag_environment, self.start, self.end)
+
+        self.assertEqual(path[0], self.start, "Start node not included in path")
+        self.assertEqual(path[-1], self.end, 'Goal node not included in path')
+        self.assertEqual(path, [
+            Node(0, 0, 0),
+            Node(1, 1, 5),
+            Node(2, 2, 45),
+            Node(3, 3, 30),
+            Node(4, 4, 0)
+        ],
+                         "Incorrect path")
+
+    def test_start_is_goal(self):
+        """
+        Test case of start and goal being the same node
+        """
+        path = self.algo.find_path(self.small_no_diag_environment, self.start, self.start)
+
+        self.assertEqual(path, [self.start])
+
+    def test_goal_unreachable(self):
+        pass
