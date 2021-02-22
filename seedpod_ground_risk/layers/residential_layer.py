17beef5fe505553eb417beaf81f42b2b5e30d018
Aliaksei Pilko <a.pilko@soton.ac.uk>
2021-02-22 14:09:16 +0000

    Allow geospatial buffering of residential geometries

--- seedpod_ground_risk/layers/residential_layer.py
+++ seedpod_ground_risk/layers/residential_layer.py
@@ -30,6 +30,7 @@ class ResidentialLayer(OSMTagLayer):
         import datashader as ds
         from holoviews.operation.datashader import rasterize
         import geoviews as gv
+        from copy import deepcopy
 
         bounds = bounds_polygon.bounds
 
@@ -52,8 +53,17 @@ class ResidentialLayer(OSMTagLayer):
                   cmap=colorcet.CET_L18,
                   colorbar=True, colorbar_opts={'title': 'Population'}, show_legend=False)
 
-        raster = rasterize(gv_polys, aggregator=ds.sum('population'),
-                           x_range=(bounds[1], bounds[3]), y_range=(bounds[0], bounds[2]), dynamic=False)
+        if 'buffer' in kwargs:
+            buffered_df = deepcopy(census_df)
+            buffered_df.geometry = buffered_df.to_crs('EPSG:27700') \
+                .buffer(kwargs['buffer']).to_crs('EPSG:4326')
+            buffered_polys = gv.Polygons(buffered_df, kdims=['Longitude', 'Latitude'], vdims=['name', 'population'])
+            raster = rasterize(buffered_polys, aggregator=ds.sum('population'),
+                               x_range=(bounds[1], bounds[3]), y_range=(bounds[0], bounds[2]), dynamic=False)
+        else:
+            raster = rasterize(gv_polys, aggregator=ds.sum('population'),
+                               x_range=(bounds[1], bounds[3]), y_range=(bounds[0], bounds[2]), dynamic=False)
+
         raster_grid = np.copy(list(raster.data.data_vars.items())[0][1].data.astype(np.float))
 
         return gv_polys, raster_grid, gpd.GeoDataFrame(census_df)
