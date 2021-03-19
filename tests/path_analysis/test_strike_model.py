6239cf54d636864a0ca79d49393b3e2ce93d7dae
Aliaksei Pilko <a.pilko@soton.ac.uk>
2021-03-19 10:28:34 +0000

    Add strike model tests

---
+++ tests/path_analysis/test_strike_model.py
@@ -0,0 +1,43 @@
+import unittest
+
+import numpy as np
+
+from seedpod_ground_risk.path_analysis.harm_models.strike_model import StrikeModel, get_lethal_area
+
+
+class StrikeModelTestCase(unittest.TestCase):
+
+    def setUp(self) -> None:
+        self.test_shape = (100, 100)
+        self.pix_area = 20 * 20
+        self.test_pdf = np.random.random(self.test_shape) * 2
+
+    def test_lethal_area_ranges(self):
+        angles = np.deg2rad(np.linspace(1, 90))
+        la = get_lethal_area(angles, 1)
+        prop_la = la / self.pix_area
+
+        self.assertLessEqual(prop_la.max(), 1)
+        self.assertGreaterEqual(prop_la.min(), 0)
+
+    def test_max_range(self):
+        max_grid = np.full(self.test_shape, 1)
+
+        sm = StrikeModel(max_grid, self.pix_area, np.deg2rad(30), 2)
+        out = sm.transform(max_grid)
+
+        self.assertLessEqual(out.max(), 1)
+        self.assertGreaterEqual(out.min(), 0)
+
+    def test_min_range(self):
+        min_grid = np.full(self.test_shape, 0)
+
+        sm = StrikeModel(min_grid, self.pix_area, np.deg2rad(30), 2)
+        out = sm.transform(min_grid)
+
+        self.assertLessEqual(out.max(), 1)
+        self.assertGreaterEqual(out.min(), 0)
+
+
+if __name__ == '__main__':
+    unittest.main()
