31c4837a856213189a10f50fa94f709674d937cd
Aliaksei Pilko <a.pilko@soton.ac.uk>
2021-03-22 11:35:48 +0000

    Refactor strike model args

--- tests/path_analysis/test_strike_model.py
+++ tests/path_analysis/test_strike_model.py
@@ -10,7 +10,7 @@ class StrikeModelTestCase(unittest.TestCase):
     def setUp(self) -> None:
         self.test_shape = (100, 100)
         self.pix_area = 20 * 20
-        self.test_pdf = np.random.random(self.test_shape) * 2
+        self.test_pdf = np.random.random(self.test_shape)
 
     def test_lethal_area_ranges(self):
         angles = np.deg2rad(np.linspace(1, 90))
@@ -23,7 +23,7 @@ class StrikeModelTestCase(unittest.TestCase):
     def test_max_range(self):
         max_grid = np.full(self.test_shape, 1)
 
-        sm = StrikeModel(max_grid, self.pix_area, np.deg2rad(30), 2)
+        sm = StrikeModel(max_grid, self.pix_area, 2)
         out = sm.transform(max_grid)
 
         self.assertLessEqual(out.max(), 1)
@@ -32,11 +32,18 @@ class StrikeModelTestCase(unittest.TestCase):
     def test_min_range(self):
         min_grid = np.full(self.test_shape, 0)
 
-        sm = StrikeModel(min_grid, self.pix_area, np.deg2rad(30), 2)
+        sm = StrikeModel(min_grid, self.pix_area, 2)
         out = sm.transform(min_grid)
 
         self.assertLessEqual(out.max(), 1)
         self.assertGreaterEqual(out.min(), 0)
+
+    def test_array_inputs(self):
+        angles = np.random.random(self.test_shape) * 2
+        pop_density = np.random.random(self.test_shape) * 6
+        sm = StrikeModel(pop_density, self.pix_area, 2)
+        out = sm.transform(self.test_pdf, impact_angle=angles)
+        self.assertTupleEqual(self.test_shape, out.shape)
 
 
 if __name__ == '__main__':
