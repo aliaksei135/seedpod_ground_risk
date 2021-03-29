46c86cc3d3d27dd77549ab933eca5061a910ccb0
Aliaksei Pilko <a.pilko@soton.ac.uk>
2021-03-29 14:20:33 +0100

    Test layer utils

---
+++ tests/layers/test_utils.py
@@ -0,0 +1,24 @@
+import unittest
+
+import shapely.geometry as sg
+
+from seedpod_ground_risk.core.plot_server import make_bounds_polygon
+
+
+class UtilsTestCase(unittest.TestCase):
+
+    def test_bounds_polygon_2arg(self):
+        res = make_bounds_polygon((1, 5), (1, 5))
+        poly = sg.Polygon([(1, 1), (1, 5), (5, 5), (5, 1)])
+        # __eq__ and .equals are not the same for sg objects!
+        self.assertTrue(res.equals(poly))
+
+    def test_bounds_polygon_4arg(self):
+        res = make_bounds_polygon(1, 1, 5, 5)
+        poly = sg.Polygon([(1, 1), (1, 5), (5, 5), (5, 1)])
+        # __eq__ and .equals are not the same for sg objects!
+        self.assertTrue(res.equals(poly))
+
+
+if __name__ == '__main__':
+    unittest.main()
