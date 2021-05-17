0a0fd1db5f55f687039e8477e2d2decead4ef4aa
Aliaksei Pilko <a.pilko@soton.ac.uk>
2021-05-17 09:31:48 +0100

    Switch over to Node class

--- seedpod_ground_risk/pathfinding/algorithm.py
+++ seedpod_ground_risk/pathfinding/algorithm.py
@@ -1,11 +1,10 @@
 import abc
-from typing import Tuple
 
-from seedpod_ground_risk.pathfinding.environment import GridEnvironment
+from seedpod_ground_risk.pathfinding.environment import GridEnvironment, Node
 
 
 class Algorithm(abc.ABC):
 
     @abc.abstractmethod
-    def find_path(self, environment: GridEnvironment, start: Tuple[int, int], goal: Tuple[int, int]):
+    def find_path(self, environment: GridEnvironment, start: Node, goal: Node):
         pass
