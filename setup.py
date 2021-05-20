e0f7f6479a1a545490de696ec1c68f504760adfc
Aliaksei Pilko <a.pilko@soton.ac.uk>
2021-05-20 11:09:14 +0100

    Bump versions

--- setup.py
+++ setup.py
@@ -112,7 +112,7 @@ def readme():
 
 setup(
     name='seedpod-ground-risk',
-    version='0.12.1',
+    version='0.13.0',
     author='Aliaksei Pilko',
     author_email='a.pilko@southampton.ac.uk',
     description='UAS ground risk analysis and path planning',
