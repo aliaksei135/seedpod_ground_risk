92a34dcb053090a4d03a364b77f04fb164bd9a5c
Zach10a <zachtait84@outlook.com>
2021-07-23 10:43:42 +0200

    Add labels to graph

--- seedpod_ground_risk/ui_resources/info_popups.py
+++ seedpod_ground_risk/ui_resources/info_popups.py
@@ -20,12 +20,15 @@ class DataWindow(QDialog):
         self.canvas = FigureCanvas(self.figure)
         self.toolbar = NavigationToolbar(self.canvas, self)
 
+        self.setWindowTitle("Path Data")
+
         layout = QVBoxLayout()
         layout.addWidget(self.toolbar)
         layout.addWidget(self.canvas)
         self.setLayout(layout)
         ax1 = self.figure.add_subplot(111)
         ys = []
+        dist = []
         for idx in range(len(path[:-1])):
             n0 = self.path[idx].position
             n1 = self.path[idx + 1].position
@@ -33,6 +36,13 @@ class DataWindow(QDialog):
             ys.append(grid[l[0], l[1]])
         ys = np.hstack(ys)
         ax1.plot(ys)
+        ax1.set_xlabel('Distance (m)')
+        ax1.set_ylabel('Risk of fatality (per hour)')
+        ax1.set_title(f"The total fatality risk over this path = {self.total_risk(ys)} per hour", pad=20)
 
         self.canvas.draw()
         self.show()
+
+    def total_risk(self, ys):
+        tot_risk = "{:.2e}".format(sum(ys))
+        return tot_risk
