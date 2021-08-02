d8bb865e5f3471068e9714087dae8bdf45d4b9c8
Zach10a <zachtait84@outlook.com>
2021-08-02 15:23:13 +0200

    Add mpl.patch code for info box

--- seedpod_ground_risk/ui_resources/info_popups.py
+++ seedpod_ground_risk/ui_resources/info_popups.py
@@ -26,7 +26,8 @@ class DataWindow(QDialog):
         layout.addWidget(self.toolbar)
         layout.addWidget(self.canvas)
         self.setLayout(layout)
-        ax1 = self.figure.add_subplot(111)
+        ax1 = self.figure.add_subplot(121)
+        ax2 = self.figure.add_subplot(122)
         ys = []
         dist = []
         for idx in range(len(path[:-1])):
@@ -40,9 +41,28 @@ class DataWindow(QDialog):
         ax1.set_ylabel('Risk of fatality (per hour)')
         ax1.set_title(f"The total fatality risk over this path = {self.total_risk(ys)} per hour", pad=20)
 
+        ax2.add_patch(p)
         self.canvas.draw()
         self.show()
 
     def total_risk(self, ys):
         tot_risk = "{:.2e}".format(sum(ys))
         return tot_risk
+
+    def create_info_patch(self):
+
+        # build a rectangle in axes coords
+        left, width = .25, .5
+        bottom, height = .25, .5
+        right = left + width
+        top = bottom + height
+
+        # axes coordinates: (0, 0) is bottom left and (1, 1) is upper right
+        p = patches.Rectangle(
+            (left, bottom), width, height,
+            fill=False, clip_on=False
+        )
+
+        return p
+
+
