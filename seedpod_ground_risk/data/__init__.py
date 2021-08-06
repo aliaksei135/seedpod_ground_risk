ae2ff97d70912369c54d8cadb7f0b353dce4a328
Aliaksei Pilko <a.pilko@soton.ac.uk>
2021-08-06 13:38:13 +0100

    Typing is hard

--- seedpod_ground_risk/data/__init__.py
+++ seedpod_ground_risk/data/__init__.py
@@ -2,35 +2,35 @@ import os
 
 
 def aircraft_list_filepath():
-    aircraft_list_fp = os.path.join(("static_data", "aircraft_list.json"))
+    aircraft_list_fp = os.path.join("static_data", "aircraft_list.json")
     return aircraft_list_fp
 
 
 def england_wa_2011_clipped_filepath():
-    england_wa_2011_clipped_fp = os.path.join(('static_data', 'england_wa_2011_clipped.shp'))
+    england_wa_2011_clipped_fp = os.path.join('static_data', 'england_wa_2011_clipped.shp')
     return england_wa_2011_clipped_fp
 
 
 def nhaps_data_filepath():
-    nhaps_data_fp = os.path.join(('static_data', 'nhaps.json'))
+    nhaps_data_fp = os.path.join('static_data', 'nhaps.json')
     return nhaps_data_fp
 
 
 def density_filepath():
-    density_fp = os.path.join(('static_data', 'density.csv'))
+    density_fp = os.path.join('static_data', 'density.csv')
     return density_fp
 
 
 def traffic_count_filepath():
-    traffic_count_fp = os.path.join(('static_data', 'dft_traffic_counts_aadf.csv'))
+    traffic_count_fp = os.path.join('static_data', 'dft_traffic_counts_aadf.csv')
     return traffic_count_fp
 
 
 def road_geometry_filepath():
-    road_geometry_fp = os.path.join(('static_data', '2018-MRDB-minimal.shp'))
+    road_geometry_fp = os.path.join('static_data', '2018-MRDB-minimal.shp')
     return road_geometry_fp
 
 
 def relative_variation_filepath():
-    relative_variation_fp = os.path.join(('static_data', 'tra0307.ods'))
+    relative_variation_fp = os.path.join('static_data', 'tra0307.ods')
     return relative_variation_fp
