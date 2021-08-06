44ade017b39f70543e7f4b08bde807e7f3758440
Aliaksei Pilko <a.pilko@soton.ac.uk>
2021-08-06 14:00:56 +0100

    Check file exists

--- seedpod_ground_risk/data/__init__.py
+++ seedpod_ground_risk/data/__init__.py
@@ -8,6 +8,7 @@ def aircraft_list_filepath():
 
 def england_wa_2011_clipped_filepath():
     england_wa_2011_clipped_fp = os.path.join('static_data', 'england_wa_2011_clipped.shp')
+    print(england_wa_2011_clipped_fp + "exists: " + str(os.path.exists(england_wa_2011_clipped_fp)))
     return england_wa_2011_clipped_fp
 
 
