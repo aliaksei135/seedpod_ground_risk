2fda211b08f75f6187dabed2e5e5bbac6439fadb
Aliaksei Pilko <a.pilko@soton.ac.uk>
2021-11-08 12:57:58 +0000

    Add artificial test environments

--- tests/pathfinding/test_data.py
+++ tests/pathfinding/test_data.py
@@ -1,5 +1,10 @@
 import numpy as np
 
+
+def dist(p1, p2):
+    return np.sqrt(np.power(abs(p1[0] - p2[0]), 2) + np.power(abs(p1[1] - p2[1]), 2))
+
+
 # 5x5
 SMALL_TEST_GRID = np.array([
     [0, 1, 1, 3, 6],
@@ -154,3 +159,55 @@ SMALL_DEADEND_TEST_GRID = np.array([
     [10, 24, 30, 30, 10],
     [25, 12, 10, 2, 0]
 ])
+
+
+def test_risk_square_grid():
+    low_val = 1e-9
+    high_val = 1e-7
+    shape = (100, 100)
+
+    grid = np.full(shape, low_val)
+    grid[int(shape[0] * 0.3):int(shape[0] * 0.7), int(shape[1] * 0.3):int(shape[1] * 0.7)] = high_val
+    return grid
+
+
+TEST_RISK_SQUARE_GRID = test_risk_square_grid()
+
+
+def test_risk_circle_grid():
+    low_val = 1e-9
+    high_val = 1e-7
+    shape = (100, 100)
+    radius = 20
+
+    centre = (shape[0] // 2, shape[1] // 2)
+    grid = np.full(shape, low_val)
+    for idx, _ in np.ndenumerate(grid):
+        if dist(centre, idx) <= radius:
+            grid[idx] = high_val
+
+    return grid
+
+
+TEST_RISK_CIRCLE_GRID = test_risk_circle_grid()
+
+
+def test_risk_circle2_grid():
+    low_val = 1e-9
+    mid_val = 1e-8
+    high_val = 1e-7
+    shape = (100, 100)
+    inner_radius = 10
+    outer_radius = 20
+
+    centre = (shape[0] // 2, shape[1] // 2)
+    grid = np.full(shape, low_val)
+    for idx, _ in np.ndenumerate(grid):
+        if dist(centre, idx) <= inner_radius:
+            grid[idx] = high_val
+        elif dist(centre, idx) <= outer_radius:
+            grid[idx] = mid_val
+    return grid
+
+
+TEST_RISK_CIRCLE2_GRID = test_risk_circle2_grid()
